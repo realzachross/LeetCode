c445c06f726676e5c4cae828f3479bb3f3388109
Zachary Ross <znr3@cornell.edu>
2022-06-07 14:16:35 -0400

    improved solution for reverse linked list 2

--- reverse-list/Reverse-Linked-ListII.py
+++ reverse-list/Reverse-Linked-ListII.py
@@ -1,56 +1,21 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-test = ListNode(1, ListNode(2, ListNode(3, None)))
-
-
-class Solution(object):
-    def reverseBetween(self, head, left, right):
-        """
-        :type head: ListNode
-        :type left: int
-        :type right: int
-        :rtype: ListNode
-        """
-        beforeLeft, afterRight = head, head
-        for i in range(right):
-            afterRight = afterRight.next
-            if i < left - 2:
-                beforeLeft = beforeLeft.next
-        if left == 1:
-            beforeLeft = None
-        if beforeLeft:
-            l = beforeLeft.next
-            beforeLeft.next = None
-        else:
-            l = head
-        prev = None
-        while l and l != afterRight:
-            temp = l.next
-            l.next = prev
-            prev = l
-            l = temp
-        head2 = head
-        while head2 and head2.next:
-            head2 = head2.next
-        if left == 1:
-            head = head2 = prev
-        else:
-            head2.next = prev
-        while prev and prev.next:
-            prev = prev.next
-        prev.next = afterRight
-        return head
-
-
-s = Solution()
-
-l = Solution.reverseBetween(s, test, 1, 3)
-
-while l:
-    print(l.val)
-    l = l.next
+def reverseBetween(self, head, left, right):
+    """
+    :type head: ListNode
+    :type left: int
+    :type right: int
+    :rtype: ListNode
+    """
+    dummy = ListNode(0, head)
+    beforeLeft, afterRight = dummy, dummy
+    for i in range(right + 1):
+        afterRight = afterRight.next
+        if i < left - 1:
+            beforeLeft = beforeLeft.next
+    prev, cur, exitCond = afterRight, beforeLeft.next, afterRight
+    while cur and cur != exitCond:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+    beforeLeft.next = prev
+    return dummy.next
