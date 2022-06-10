8bdbdfa5453d18f215ec83f84c645ab406ea26dc
Zachary Ross <znr3@cornell.edu>
2022-06-10 12:51:47 -0400

    added solution for reverse nodes in k groups

---
+++ reverse-list/Reverse-Nodes-In-K-Groups.py
@@ -0,0 +1,36 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if k == 1:
+            return head
+        dummy, cur, inc = ListNode(0, head), head, 0
+        dummy2 = dummy
+        while True:
+            lead = cur
+            for i in range(k - 1):
+                if lead and lead.next:
+                    lead = lead.next
+                    if inc > 0:
+                        dummy2 = dummy2.next
+                else:
+                    return dummy.next
+            prev = lead.next
+            for i in range(k):
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+            dummy2.next = prev
+            dummy2 = dummy2.next
+            inc += 1
+        return dummy.next
